9bb82a25f248aa36a6d7716e00132543a7604351
KarlLeen <limlamleen@gmail.com>
2025-11-04 17:40:51 +0800

    添加 Header 下方的分隔线：在 sidebar 和 header.content 之间添加 #2B3740 背景色的分隔线

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,6 +41,9 @@ const Header: React.FC = () => {
         </div>
       </div>
       </div>
+      
+      {/* Horizontal divider between sidebar and header content */}
+      <div className="w-full h-px bg-[#2B3740]"></div>
     </header>
   );
 };
